1b1af34ff44dd6c50c3cb355e6976045d0e77e82
PixelPointer <rafaeldcmrmc@gmail.com>
2025-11-28 20:58:24 -0400

    se creo la pantalla de inicio

--- lib/pantallas/inicio.dart
+++ lib/pantallas/inicio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyectofinal_grupo10_avansado/pantallas/reportar.dart';
 
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
@@ -10,9 +11,90 @@ class Inicio extends StatefulWidget {
 class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Center(
-        child: Text('INICIO'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('INICIO', style: TextStyle(fontWeight: FontWeight.bold) ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: const Text(
+              'ACCESO RAPIDO',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {},
+                  child: const 
+                    Text('Llamada a Emergencias',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  onPressed: (){
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HacerReporte(),
+                    ),
+                  );
+                  },
+                  child: const 
+                    Text('Realizar Reporte', 
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: const Text(
+              'ÚLTIMOS REPORTES DEL DÍA',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+
+          Expanded(
+            child: ListView(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.report, color: Colors.red),
+                  title: Text('Robo en la calle 123'),
+                  subtitle: Text('Reportado hace 2 horas'),
+                ),
+              ],
+            ),
+          ),
+
+
+        ],
       ),
     );
   }
